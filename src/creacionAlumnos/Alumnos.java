d3f1760702097c303897c911e13164df868e1554
the_p <the_p@LAPTOP-77GCKEU2>
2022-05-09 09:23:20 +0200

    Primer commit

---
+++ src/creacionAlumnos/Alumnos.java
@@ -0,0 +1,8 @@
+package creacionAlumnos;
+
+public class Alumnos {
+	
+	//Aquí hay que hacer la parte de los Alumnos, con los getter, setter y el toString
+	//Parte de Isma
+
+}
---
+++ src/creacionAlumnos/PruebaAlumno.java
@@ -0,0 +1,29 @@
+package creacionAlumnos;
+
+public class PruebaAlumno {
+
+	public static void main(String[] args) {
+		// Aquí va el menú con un switch --> Ari
+
+	}
+	
+	//Aquí van los métodos
+	public static void addAlumno() {
+		// Isma - igual que la clase de amachon
+	}
+	
+	public static void deleteAlumno() {
+		//Paola
+	
+	}
+	
+	public static void mostrarAlumno() {
+		//Ari - igual que la clase de amachon
+		
+	}
+	
+	public static void guardarAlumno() {
+		//Paola
+	
+	}
+}
